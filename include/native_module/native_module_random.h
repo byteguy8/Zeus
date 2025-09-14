042a5a808b086f03ec5446fef8540c04bad6d819
byteguy8 <byteguy8@gmail.com>
2025-09-14 18:45:34 -0400

    feat: new random native module function

--- include/native_module/native_module_random.h
+++ include/native_module/native_module_random.h
@@ -10,8 +10,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+XOShiro256 xos256;
 NativeModule *random_native_module = NULL;
 
-void random_module_init(Allocator *allocator){}
+Value native_fn_random_next(uint8_t argsc, Value *values, Value *target, void *context){
+    return INT_VALUE(xoshiro256_next(&xos256));
+}
+
+void random_module_init(Allocator *allocator){
+    xos256 = xoshiro256_init();
+    random_native_module = factory_create_native_module("random", allocator);
+
+    factory_add_native_fn("next", 0, native_fn_random_next, random_native_module, allocator);
+}
 
 #endif
