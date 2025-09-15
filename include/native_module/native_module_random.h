8c62e2c771ffccd3c388e81044c8f68fc704c7da
byteguy8 <byteguy8@gmail.com>
2025-09-14 21:35:41 -0400

    feat: new random native module function 'seed'

--- include/native_module/native_module_random.h
+++ include/native_module/native_module_random.h
@@ -13,6 +13,12 @@
 XOShiro256 xos256;
 NativeModule *random_native_module = NULL;
 
+Value native_fn_random_seed(uint8_t argsc, Value *values, Value *target, void *context){
+    uint64_t seed = (uint64_t)validate_value_int_arg(&values[0], 1, "seed", VMU_VM);
+    xos256 = xoshiro256_init_seed(seed);
+    return EMPTY_VALUE;
+}
+
 Value native_fn_random_next(uint8_t argsc, Value *values, Value *target, void *context){
     return INT_VALUE(xoshiro256_next(&xos256));
 }
@@ -21,6 +27,7 @@ void random_module_init(Allocator *allocator){
     xos256 = xoshiro256_init();
     random_native_module = factory_create_native_module("random", allocator);
 
+    factory_add_native_fn("seed", 1, native_fn_random_seed, random_native_module, allocator);
     factory_add_native_fn("next", 0, native_fn_random_next, random_native_module, allocator);
 }
 
