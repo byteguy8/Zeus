82605b4cdacbfa357d8b32df1d7ccb028c79e78b
byteguy8 <byteguy8@gmail.com>
2025-09-15 02:42:47 -0400

    refactor[native_module]: remove unused declarations

--- include/native_module.h
+++ include/native_module.h
@@ -4,15 +4,6 @@
 #include "memory.h"
 #include "native_fn.h"
 
-typedef enum native_module_symbol_type{
-	NATIVE_FUNCTION_NMSYMTYPE
-}NativeModuleSymbolType;
-
-typedef struct native_module_symbol{
-	NativeModuleSymbolType type;
-    Value value;
-}NativeModuleSymbol;
-
 typedef struct native_module{
 	char *name;
 	LZOHTable *symbols;
